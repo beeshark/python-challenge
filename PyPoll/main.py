17ef98853fcefd709daa2b5008531d9844faed28
Sara <sbehnke33@yahoo.com>
2020-08-22 12:57:08 -0500

    Update main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+# PyPoll
+
+import os
+import csv
+
+#directory ... having trouble with this one showing up with "..", "Resources" etc.
+cvspath = "/Users/Sara/Documents/GitHub/python-challenge/PyPoll/Resources/election_data.csv"
