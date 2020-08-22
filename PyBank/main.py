60abee3dae30142d28236ba7bd5a959cd72f5964
Sara <sbehnke33@yahoo.com>
2020-08-21 22:58:24 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,20 @@
+# PyBank
+
 import os
 import csv
 
+cvspath = os.path.join("Resurces", "budget_data.csv")
+#
+with open(cvspath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+
+#store data and variables
+profits = []
+net_profits = 0
+dates = []
+total_months = 0
+monthly_changes = []
+changes = 0
 
 
