fc7c208267f5c50e78cfabc4c8c62a3544a251c4
Sara <sbehnke33@yahoo.com>
2020-08-22 21:12:46 -0500

    Update main.py
    very stuck

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,12 +11,50 @@ vote_tally = 0
 votesList = []
 vote_percent = []
 candidateList =[]
-indv_candidates = []
+indv_candidate = []
+ccount = {}
 
 with open(cvspath, newline="") as csvfile:
   csvreader = csv.reader(csvfile, delimiter=",")
   csv_header = next(csvreader)
+  
+  for row in csvreader:
+    #votes cast
+    vote_tally = vote_tally + 1
 
-for row in csvreader:
-    #votes
-    vote_tally
+    #list of candidates who got votes
+    indv_candidate = row[2]
+
+    if indv_candidate in candidateList:
+        candidateList.append(row[2])
+
+
+    votesList.append(row[2])
+  #for indv_candidate in candidateList:
+     # ccount.append(votesList.count(indv_candidate))
+     # vote_percent.append(round(votesList.count(indv_candidate)/vote_tally*100,2))
+
+
+    #percentage of votes each candidate won
+
+    #election winner based on pop vote
+#winning_vote_count = max(votesList)
+#winner = str(range(indv_candidate[votesList.index(winning_vote_count)], 2))
+#string index out of range
+
+    
+
+#print here
+
+
+#output to text file here
+
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {vote_tally}")
+for i in range(len(indv_candidate)):
+    print(f"{indv_candidate[i]}: {vote_percent[i]}% ({ccount[i]})")
+    #said list index oout of range. not sure how to fix
+print("----------------------------")
+#print(f"Winner: {winner}")
+print("----------------------------")
