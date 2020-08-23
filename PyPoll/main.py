3f4f8120ff8126f4d36c34fa38f15ef93224de86
Sara <sbehnke33@yahoo.com>
2020-08-22 20:09:50 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,3 +5,18 @@ import csv
 
 #directory ... having trouble with this one showing up with "..", "Resources" etc.
 cvspath = "/Users/Sara/Documents/GitHub/python-challenge/PyPoll/Resources/election_data.csv"
+
+# store values and lists
+vote_tally = 0
+votesList = []
+vote_percent = []
+candidateList =[]
+indv_candidates = []
+
+with open(cvspath, newline="") as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=",")
+  csv_header = next(csvreader)
+
+for row in csvreader:
+    #votes
+    vote_tally
